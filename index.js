b9c1d265b545aeddadf2fc9f22dd9e644b22a081
Rob Loach <robloach@gmail.com>
2016-12-09 13:27:45 -0500

    Add xo as coding style (#37)

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-'use strict';
+'use strict'
 
-var bless = require('bless');
+var bless = require('bless')
 
-exports.name = 'bless';
-exports.inputFormats = ['css', 'bless'];
-exports.outputFormat = 'json';
+exports.name = 'bless'
+exports.inputFormats = ['css', 'bless']
+exports.outputFormat = 'json'
 
 exports.render = function (str, options) {
-  var res = bless.chunk(str, options);
-  return JSON.stringify(res.data, null, 2);
-};
+  var res = bless.chunk(str, options)
+  return JSON.stringify(res.data, null, 2)
+}
 
 exports.renderFileAsync = function (fp, options) {
   return bless.chunkFile(fp, options).then(function (res) {
-    return JSON.stringify(res.data, null, 2);
-  });
-};
+    return JSON.stringify(res.data, null, 2)
+  })
+}
